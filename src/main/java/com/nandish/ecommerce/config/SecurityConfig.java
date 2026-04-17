ac52a100786d4ad8c52999f415163ff234d37c04
nandish1302 <nandishkumarhnrenuka@gmail.com>
2026-04-17 23:14:57 +0530

    tested api's in swagger

---
+++ src/main/java/com/nandish/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.nandish.ecommerce.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+  @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http ) throws Exception{
+      http
+              .csrf(csrf -> csrf.disable())
+              .authorizeHttpRequests(auth -> auth
+                      .requestMatchers("/auth/**").permitAll()
+                      .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                      .anyRequest().authenticated()
+              );
+
+      return http.build();
+  }
+}
