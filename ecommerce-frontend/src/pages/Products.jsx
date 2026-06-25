26de0e9ced9da5715bc5d3060d1020bfb7e3fa8a
nandish1302 <nandishkumarhnrenuka@gmail.com>
2026-06-25 22:01:37 +0530

    Made changes in Product

--- ecommerce-frontend/src/pages/Products.jsx
+++ ecommerce-frontend/src/pages/Products.jsx
@@ -1,31 +1,50 @@
 import Navbar from "../components/Navbar";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-
 const Products = () => {
-  const[products, setProducts] = useState([]);
-  const[loading, setLoading] = useState(true);
-  const[error, setError] = useState("");
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-  console.log("Products page opened");
-}, []);
-const fetchProducts = async () => {
-  try {
-    const response = await axios.get(
-      "https://fakestoreapi.com/products"
-    );
+    fetchProducts();
+  }, []);
 
-    setProducts(response.data);
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(
+        "https://fakestoreapi.com/products"
+      );
 
-  } catch (error) {
-    console.log(error);
+      setProducts(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError("Failed to load products");
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <h1>Loading Products...</h1>;
   }
-};
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
   return (
-    <>
+    <>  
       <Navbar />
+
       <h1>Products Page</h1>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>₹ {product.price}</p>
+        </div>
+      ))}
     </>
   );
 };
